44aa1c353d7e31c742c2c8674f957b49c25a55bf
Shahedul Alam <75212836+programmershahed@users.noreply.github.com>
2021-03-03 04:14:50 +0600

    user Registration design
    user Registration design

--- transportation-service/app/Http/Controllers/UserregController.php
+++ transportation-service/app/Http/Controllers/UserregController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class UserregController extends Controller
 {
-    //
+    public function userregistation(){
+        return view(view:'user.userreg');
+       }
 }
--- transportation-service/routes/web.php
+++ transportation-service/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserregController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/userRegistation', [UserregController::class, 'userregistation']);
