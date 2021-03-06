a5f2fd713e3c53066c4864ce8157598886b52982
Shahedul Alam <75212836+programmershahed@users.noreply.github.com>
2021-03-06 22:17:28 +0600

    Homepage design
    Homepage design

---
+++ transportation-service/resources/views/home.blade.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Eforlad travel</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href=" {{asset('assets/homepage/css/bootstrap.min.css')}}"> 
+      <!-- style css -->
+      <link rel="stylesheet" href="{{asset('assets/homepage/css/style.css')}}">  
+      <!-- Responsive-->
+      <link rel="stylesheet" href="{{asset('assets/homepage/css/responsive.css')}}"> 
+      <!-- fevicon -->
+      <link rel="icon" href="{{asset('assets/homepage/images/fevicon.png')}}" type="image/gif" /> 
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href=" {{asset('assets/homepage/css/jquery.mCustomScrollbar.min.css')}}">
+      <!-- Tweaks for older IEs-->
+      <!-- owl stylesheets --> 
+      <link rel="stylesheet" href="{{asset('assets/homepage/css/owl.carousel.min.css')}}">  
+      <link rel="stylesheet" href="{{asset('assets/homepage/css/owl.theme.default.min.css')}}">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="{{asset('assets/homepage/images/loading.gif')}}" alt="#" /></div> 
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="header_white_section">
+               <div class="container-fluid">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="header_information">
+                           <ul>
+                              <li><img src="{{asset('assets/homepage/images/1.png')}}" alt="#"/> 145.street road new York</li> 
+                              <li><img src="{{asset('assets/homepage/images/2.png')}}" alt="#"/> +71  5678954378</li>
+                              <li><img src="{{asset('assets/homepage/images/3.png')}}" alt="#"/> Demo@hmail.com</li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo"> <a href="index.html"><img src=" {{asset('assets/homepage/images/logo.png')}}" alt="#"></a> </div>
+                        </div> 
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="menu-area">
+                        <div class="limit-box">
+                           <nav class="main-menu">
+                              <ul class="menu-area-main">
+                                 <li class="active"> <a href="#">Home</a> </li>
+                                 <li><a href="#travel">Privacy Policy
+                                </a></li>
+                                 <li><a href="#blog">Contact us</a></li>
+                                 <li><a href="#contact">About us</a></li>
+                                 <li><a href="#contact">FAQ // Help</a></li>
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
+      <section >
+         <div class="banner-main">
+            <img src=" {{asset('assets/homepage/images/banner.jpg')}}" alt="#"/> 
+            <div class="container">
+               <div class="text-bg">
+                  <h1> <br><strong class="white">REQUEST A SERVICE</strong></h1>
+                 
+                  <div class="container">
+                     <form class="main-form">
+                        <h3>Find Your Service</h3>
+                        <div class="row">
+                           <div class="col-md-9">
+                              <div class="row">
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label >From</label>
+                                    <input class="form-control" placeholder="" type="text" name="">
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label > Type of items</label>
+                                    <select class="form-control" name="Any">
+                                       <option>Documents</option>
+                                       <option>Vehicles</option>
+                                       <option>Removals</option>
+                                       <option>Others</option>
+                                    </select>
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label >Postal Code</label>
+                                    <input class="form-control" placeholder="" type="text" name="00.0">
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label >To</label>
+                                    <input class="form-control" placeholder="Any" type="text" name="Any">
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label >Date</label>
+                                    <input class="form-control" placeholder="Any" type="date" name="Any">
+                                 </div>
+                                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                                    <label >Community</label>
+                                    <input class="form-control" placeholder="" type="text" name="00.0">
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
+                              <a href="#">Next</a>
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+     
+      <!-- traveling -->
+      <div id="travel" class="traveling">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 ">
+                  <div class="titlepage">
+                     <h2>Select Services For Request</h2>
+                     <span>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</span> 
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="traveling-box">
+                     <i><img src=" {{asset('assets/homepage/icon/travel-icon.png')}}" alt="icon"/></i> 
+                     <h3>Documents</h3>
+                     <p> going to use a passage of Lorem Ipsum, you need to be </p>
+                     <div class="read-more">
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="traveling-box">
+                     <i><img src=" {{asset('assets/homepage/icon/travel-icon2.png')}}" alt="icon"/></i> 
+                     <h3> Removals</h3>
+                     <p> going to use a passage of Lorem Ipsum, you need to be </p>
+                     <div class="read-more">
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="traveling-box">
+                     <i><img src="{{asset('assets/homepage/icon/travel-icon3.png')}}" alt="icon"/></i>
+                     <h3>Vehicles</h3>  
+                     <p> going to use a passage of Lorem Ipsum, you need to be </p>
+                     <div class="read-more">
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="traveling-box">
+                     <i><img src=" {{asset('assets/homepage/icon/travel-icon4.png')}}" alt="icon"/></i>
+                     <h3>Others</h3>
+                     <p> going to use a passage of Lorem Ipsum, you need to be </p>
+                     <div class="read-more">
+                        <a  href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end traveling -->
+      
+      <!--London -->
+     
+      <!-- end London -->
+      <!--Tours -->
+      
+      <!-- end Tours -->
+      <!-- Amazing -->
+      <div class="amazing">
+        <div class="container">
+           <div class="row">
+              <div class="col-md-12">
+                 <div class="amazing-box">
+                    <h2>Advertising banner</h2>
+                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</span>
+                   <a href="#">Get More</a>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </div>
+      <!-- end Amazing -->
+      <!-- our blog -->
+     
+      <!-- end our blog -->
+       <!-- about -->
+       <div id="about" class="about">
+        <div class="container">
+           <div class="row">
+              <div class="col-md-12 ">
+                 <div class="titlepage">
+                    <h2>About  Us</h2>
+                    <span> fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</span>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div class="bg">
+           <div class="container">
+              <div class="row">
+                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div class="about-box">
+                       <p> <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure thereThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</span></p>
+                       
+                    </div>
+                 </div>
+              </div>
+           </div>
+           <a href="#">Read More</a>
+        </div>
+     </div>
+      <!-- end about -->
+      <!-- footer -->
+      <footer>
+         <div id="contact" class="footer">
+            <div class="container">
+               <div class="row pdn-top-30">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                     <ul class="location_icon">
+                        <li> <a href="#"><img src="{{asset('assets/homepage/icon/facebook.png')}}"></a></li> 
+                        <li> <a href="#"><img src="{{asset('assets/homepage/icon/Twitter.png')}}"></a></li>
+                        <li> <a href="#"><img src="{{asset('assets/homepage/icon/linkedin.png')}}"></a></li>
+                        <li> <a href="#"><img src="{{asset('assets/homepage/icon/instagram.png')}}"></a></li>
+                     </ul>
+                  </div>
+                  <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                     <div class="Follow">
+                        <h3>CONTACT US</h3>
+                        <span>123 Second Street Fifth <br>Avenue,<br>
+                        Manhattan, New York<br>
+                        +987 654 3210</span>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                     <div class="Follow">
+                        <h3>ADDITIONAL LINKS</h3>
+                        <ul class="link">
+                           <li> <a href="#">About us</a></li>
+                           <li> <a href="#">Terms and conditions</a></li>
+                           <li> <a href="#"> Privacy policy</a></li>
+                           <li> <a href="#">FAQ // Help
+                        </a></li>
+                           <li> <a href="#"> Contact us</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                     <div class="Follow">
+                        <h3> Contact</h3>
+                        <div class="row">
+                           <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                              <input class="Newsletter" placeholder="Name" type="text">
+                           </div>
+                           <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                              <input class="Newsletter" placeholder="Email" type="text">
+                           </div>
+                           <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                              <textarea class="textarea" placeholder="comment" type="text">Comment</textarea>
+                           </div>
+                        </div>
+                        <button class="Subscribe">Submit</button>
+                     </div>
+                  </div>
+               </div>
+               <div class="copyright">
+                  <div class="container">
+                     <p>Copyright 2019 All Right Reserved By <a href="https://html.design/">Free html Templates</a></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="{{asset('assets/homepage/js/jquery.min.js')}}"></script> 
+      <script src="{{asset('assets/homepage/js/popper.min.js')}}"></script> 
+      <script src="{{asset('assets/homepage/js/bootstrap.bundle.min.js')}}"></script>
+      <script src="{{asset('assets/homepage/js/jquery-3.0.0.min.js')}}"></script> 
+      <script src="{{asset('assets/homepage/js/plugin.js')}}"></script> 
+      <!-- sidebar -->
+      <script src="{{asset('assets/homepage/js/jquery.mCustomScrollbar.concat.min.js')}}"></script>  
+      <script src="{{asset('assets/homepage/js/custom.js')}}"></script>  
+      <!-- javascript --> 
+      <script src="{{asset('assets/homepage/js/owl.carousel.js')}}"></script> 
+      <script>
+         $(document).ready(function() {
+           var owl = $('.owl-carousel');
+           owl.owlCarousel({
+             margin: 10,
+             nav: true,
+             loop: true,
+             responsive: {
+               0: {
+                 items: 1
+               },
+               600: {
+                 items: 2
+               },
+               1000: {
+                 items: 3
+               }
+             }
+           })
+         })
+      </script>
+     
+   </body>
+</html>
--- transportation-service/routes/web.php
+++ transportation-service/routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\UserregController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/userRegistation', [UserregController::class, 'userregistation']);
